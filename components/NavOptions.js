4198ec48be3626c11fae8c4154a4a35fac7b3d39
Pratik Karanjit <pratikkaranjit@gmail.com>
2024-02-11 09:33:53 +0545

    NavOptions file and tw

---
+++ components/NavOptions.js
@@ -0,0 +1,48 @@
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import tw from "tailwind-react-native-classnames";
+import { Icon } from "react-native-elements";
+
+const data = [
+  {
+    id: "123",
+    title: "Get a ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
+  {
+    id: "456",
+    title: "Order food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatsScreen",
+  },
+];
+
+const NavOptions = () => {
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      horizontal
+      renderItem={({ item }) => (
+        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+          <View>
+            <Image
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+              source={{ uri: item.image }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              name="arrowright"
+              color="white"
+              type="antdesign"
+            />
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
+
+export default NavOptions;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
+import NavOptions from "../components/NavOptions";
 
 const HomeScreen = () => {
   return (
@@ -17,6 +18,8 @@ const HomeScreen = () => {
             uri: "https://links.papareact.com/gzs",
           }}
         />
+
+        <NavOptions></NavOptions>
       </View>
     </SafeAreaView>
   );
